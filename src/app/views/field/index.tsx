68f970218dd61c4108abb7fdf07db98c8d7571b1
月牙魔法师 <617938514@qq.com>
2026-01-19 00:02:56 +0800

    界面优化

--- src/app/views/field/index.tsx
+++ src/app/views/field/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-// 中文注释：野外刷怪视图的属性接口定义
-export interface FieldProps {
-  // 中文注释：预留属性（如刷怪配置、状态等）
-}
+export interface FieldProps {}
 
-// 中文注释：野外刷怪视图组件
 const FieldView: React.FC<FieldProps> = () => {
   return (
     <section className="card">
